afb2d5c9e8574ad5932575df8bb2ab8ea3eae508
Julia Owens <juliasowens@gmail.com>
2020-03-19 14:21:52 -0400

    checkpoint-13-revision

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 function onReady() {
-  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const toDoList = document.getElementById('toDoList');
-
+  let toDos = [];
   let id = 0;
-
-
-  function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) {return;}
-
-    toDos.push({
-      title: newToDoText.value,
-      complete: false,
-
-      id: id
-    });
-    newToDoText.value = '';
-    console.log(id);
-    renderTheUI();
-  }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
-      const deleteButton = document.createElement('button');
-
       checkbox.type = "checkbox";
-      deleteButton.type = "button";
-      deleteButton.innerHTML = "Delete";
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
+
+      deleteButton.addEventListener ('click', event =>{
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
 
       newLi.textContent = toDo.title;
 
@@ -41,37 +28,29 @@ function onReady() {
       newLi.appendChild(checkbox);
       newLi.appendChild(deleteButton);
 
-      deleteButton.addEventListener ("click", event =>{
-        event.preventDefault();
-
-        function removeItem() {
-          for (var i = 0; i < toDos.length; i++ ){
-               if( toDo.id < i && id == i){
-                 return true;
-               }else{
-                 event.target.parentElement.remove()
-               }
-            }
-        }
-        console.log(removeItem());
-        console.log(toDo.id);
-        console.log( toDos.filter(removeItem));
-        return toDos.filter(removeItem);
-        renderTheUI()
-      });
-
-
-    });
+    })
   }
 
+function createNewToDo() {
+  const newToDoText = document.getElementById('newToDoText');
+  if (!newToDoText.value) {return;}
+  toDos.push({
+    title: newToDoText.value,
+    complete: false,
+    id:id
+  });
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-    createNewToDo();
+  id++;
+
+  newToDoText.value = '';
+  renderTheUI();
+}
+addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
   });
 
   renderTheUI();
-
 }
 
 window.onload = function() {
